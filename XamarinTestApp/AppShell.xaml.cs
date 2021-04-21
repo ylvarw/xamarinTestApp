1e64f30c83472e8334ffb94a01676acbfd527ccf
ylvarw <ylvarw@gmail.com>
2021-04-21 14:31:06 +0200

    removed example code

--- XamarinTestApp/AppShell.xaml.cs
+++ XamarinTestApp/AppShell.xaml.cs
@@ -9,8 +9,6 @@ namespace XamarinTestApp
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
             Routing.RegisterRoute(nameof(XamarinTestApp.Views.CpuEntryPage), typeof(XamarinTestApp.Views.CpuEntryPage));
             Routing.RegisterRoute(nameof(XamarinTestApp.Views.GpuEntryPage), typeof(XamarinTestApp.Views.GpuEntryPage));
             Routing.RegisterRoute(nameof(XamarinTestApp.Views.RenderEntryPage), typeof(XamarinTestApp.Views.RenderEntryPage));
